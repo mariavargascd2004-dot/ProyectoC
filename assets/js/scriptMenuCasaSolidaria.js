00d7dc648ceff114c7cc9d85d2b0ecb4cc1fabe0
mariavargascd2004-dot <mariavargascd2004@gmail.com>
2025-10-05 18:30:20 -0300

    Creacion del Menu para Aceptar o Rechazar los registros de nuevos Emprendedores

--- assets/js/scriptMenuCasaSolidaria.js
+++ assets/js/scriptMenuCasaSolidaria.js
@@ -1,5 +1,71 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    // Animar campana de notificaciones
+    const btnFlotante__LojaSolidaria = document.getElementById("btnFlotante--LojaSolidaria");
+
+    setInterval(() => {
+        btnFlotante__LojaSolidaria.style.animation = "moverCampana 0.6s ease";
+        setTimeout(() => {
+            btnFlotante__LojaSolidaria.style.animation = "none";
+        }, 600);
+    }, 3000);
+
+    /* Modal Aceptar Registros de Emprendedores  */
+    const modal__botonMostrarMas = document.querySelector(".modal--botonMostrarMas");
+    const modal__NuevoEmprendedor = document.querySelector(".modal--NuevoEmprendedor");
+    const modal__contenidoMasInfo = document.querySelector(".modal--contenidoMasInfo");
+    modal__botonMostrarMas.addEventListener("click", function () {
+        if (modal__contenidoMasInfo.style.display != "block") {
+            this.innerHTML = "Mostrar detalhes <i class='fa-solid fa-angle-up'></i>";
+            modal__contenidoMasInfo.style.display = "block";
+        }
+        else {
+            this.innerHTML = "Mostrar detalhes <i class='fa-solid fa-angle-down'></i>";
+            modal__contenidoMasInfo.style.display = "none";
+        }
+    })
+
+
+    const modal__botonRechazar = document.querySelector(".modal__botonRechazar");
+    const modal__botonAceptar = document.querySelector(".modal__botonAceptar");
+    // Botão Aceitar
+    modal__botonAceptar.addEventListener("click", () => {
+        modal__NuevoEmprendedor.remove();
+        Swal.fire({
+            icon: 'success',
+            title: 'Registro Aceito',
+            text: 'O novo empreendedor foi aceito.',
+            confirmButtonColor: '#DCA700',
+            confirmButtonText: 'Ok'
+        });
+    });
+    // Botão Rejeitar
+    modal__botonRechazar.addEventListener("click", () => {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Tem certeza?',
+            text: 'O registro do empreendedor será rejeitado.',
+            showCancelButton: true,
+            confirmButtonColor: '#B2442E',
+            cancelButtonColor: '#6c757d',
+            confirmButtonText: 'Sim, rejeitar',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                modal__NuevoEmprendedor.remove();
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Registro Rejeitado',
+                    text: 'O registro do empreendedor foi rejeitado.',
+                    confirmButtonColor: '#DCA700',
+                    confirmButtonText: 'Ok'
+                });
+            }
+        });
+    });
+
+
+
     /* Cambiar el Pooster Inicial - Mostrar más infomración de la historia */
     let portada__botao = document.getElementById("portada__botao");
     let portada__imagem = document.querySelector(".portada__imagem");
